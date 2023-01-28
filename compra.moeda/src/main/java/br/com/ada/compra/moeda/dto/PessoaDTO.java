db70b7e27469fc9eae6183296b90a7b4f5dccd30
Jessica <jsouzasouza120@gmail.com>
2023-01-28 17:12:32 -0300

    Correção da entidade PessoaDTO

--- compra.moeda/src/main/java/br/com/ada/compra/moeda/dto/PessoaDTO.java
+++ compra.moeda/src/main/java/br/com/ada/compra/moeda/dto/PessoaDTO.java
@@ -1,24 +1,35 @@
 package br.com.ada.compra.moeda.dto;
 
 import br.com.ada.compra.moeda.model.Pessoa;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDate;
-    @Data
-    public class PessoaDTO {
-        private final String cpf;
-        private final String nome;
-        private LocalDate nascimento;
 
-        public static PessoaDTO of(Pessoa pessoa) {
-            PessoaDTO dto = new PessoaDTO(pessoa.getCpf(), pessoa.getNome());
-            dto.nascimento = pessoa.getNascimento();
-            return dto;
-        }
+@Data
+public class PessoaDTO {
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+    private String estadoCivil;
+    private String sexo;
 
-        public Pessoa toEntity() {
-            Pessoa entity = new Pessoa(cpf, nome);
-            entity.setNascimento(nascimento);
-            return entity;
-        }
+    public static PessoaDTO of(Pessoa pessoa) {
+        PessoaDTO dto = new PessoaDTO();
+        dto.nome = pessoa.getNome();
+        dto.cpf = pessoa.getCpf();
+        dto.nascimento = pessoa.getNascimento();
+        dto.estadoCivil = pessoa.getEstadoCivil();
+        dto.sexo = pessoa.getSexo();
+        return dto;
+    }
+
+    public Pessoa toEntity() {
+        Pessoa entity = new Pessoa();
+        entity.setNome(nome);
+        entity.setCpf(cpf);
+        entity.setNascimento(nascimento);
+        entity.setEstadoCivil(estadoCivil);
+        entity.setSexo(sexo);
+        return entity;
+    }
 }
