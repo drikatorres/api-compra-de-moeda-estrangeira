53326fc133106550e744c173141a567c142df7c6
Jessica <jsouzasouza120@gmail.com>
2023-02-04 12:38:51 -0300

    Ajuste da entidade Compra

--- compra.moeda/src/main/java/br/com/ada/compra/moeda/model/Compra.java
+++ compra.moeda/src/main/java/br/com/ada/compra/moeda/model/Compra.java
@@ -1,57 +1,18 @@
 package br.com.ada.compra.moeda.model;
 
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 public class Compra {
     @Id
+    private String id;
     private String cpf;
     private Moeda moeda;
     private BigDecimal valor;
     private String agencia;
-
-    public Compra(String cpf, Moeda moeda, BigDecimal valor, String agencia) {
-        this.cpf = cpf;
-        this.moeda = moeda;
-        this.valor = valor;
-        this.agencia = agencia;
-    }
-
-    public Compra() {
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Moeda getMoeda() {
-        return moeda;
-    }
-
-    public void setMoeda(Moeda moeda) {
-        this.moeda = moeda;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public String getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(String agencia) {
-        this.agencia = agencia;
-    }
 }
